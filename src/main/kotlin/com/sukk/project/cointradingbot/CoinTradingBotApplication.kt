9c75108ba840dfa0a2c8c433e5245226a75c06ae
echo(김환석) <echo.ohce@kakaocorp.com>
2021-11-02 16:02:51 +0900

    first commit

---
+++ src/main/kotlin/com/sukk/project/cointradingbot/CoinTradingBotApplication.kt
@@ -0,0 +1,11 @@
+package com.sukk.project.cointradingbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoinTradingBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoinTradingBotApplication>(*args)
+}
